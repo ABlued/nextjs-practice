90e37b902702ba984630eb29653b0f9390f0b042
나재완 <skwodhkswkd@naver.com>
2025-05-03 15:57:54 +0900

    feat: Meal 상세 페이지 구현

---
+++ 05-onwards-foodies-starting-project/app/meals/[mealSlug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div className='not-found'>
+      <h1>Meal Not Found!</h1>
+      <p>Unfortunately, we could not find the requested page or resource</p>
+    </div>
+  );
+}
